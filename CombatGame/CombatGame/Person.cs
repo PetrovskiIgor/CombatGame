1ca15562a2143b99be5e75e5de2f5eb0b19d75f4
VikiPeeva <peeva_viki@yahoo.com>
2014-05-02 15:33:44 +0200

    PersonVerzija

---
+++ CombatGame/CombatGame/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CombatGame
+{
+    public enum State
+    {
+
+    }
+    class Person
+    {
+        String Name { get; set; }
+        int Health { get; set; }
+        PictureBox pbPlayer;
+    }
+}
